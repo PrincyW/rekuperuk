f5c9807219b322ea7079d876fe33ac7da5ac0f5e
Vasiliki Georgiou <vicky.georgiou2012@gmail.com>
2024-03-11 17:48:06 +0100

    Adding config for Sweel Alert in donation form WIP, correcting About Us page, adding texts

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 pin "flatpickr" # @4.6.13
+pin "sweetalert2", to: "https://ga.jspm.io/npm:sweetalert2@11.7.2/dist/sweetalert2.all.js" # @11.10.6
