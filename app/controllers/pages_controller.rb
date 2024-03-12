cc908cbf6972089f377abfbd0b9a60fd9a232ae2
Vasiliki Georgiou <vicky.georgiou2012@gmail.com>
2024-03-12 09:50:20 +0100

    Adding corrections, creating route and splash page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
+  def splash
+  end
+
   def home
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "pages#splash"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
+  get '/home', to: 'pages#home'
   get '/about', to: 'pages#about_us'
 
   resources :donations, only: [:new, :create]
